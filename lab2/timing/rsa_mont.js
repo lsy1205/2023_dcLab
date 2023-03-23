f4ee774b272e63e17c45754ec500f8035b31fbb8
FrSh28 <37129124+FrSh28@users.noreply.github.com>
2023-03-23 19:28:28 +0800

    add timing diagram

---
+++ lab2/timing/rsa_mont.js
@@ -0,0 +1,10 @@
+{
+    signal: [
+        {name: "clk",     wave: "p........."},
+        {name: "start",   wave: "010......."},
+        {name: "m_r",     wave: "0..======.", data: ["b","b2","b4","b8","b16", "m_result"]},
+        {name: "counter", wave: "0..======0", data: [1,2,3,4,5,6]},
+        {name: "o_fin",   wave: "0.......10",},
+
+    ]
+}
---
+++ lab2/timing/rsa_prep.js
@@ -0,0 +1,10 @@
+{
+    signal: [
+        {name: "clk",     wave: "p......."},
+        {name: "start",   wave: "010....."},
+        {name: "m_r",     wave: "0.=====.", data: ["b","2b","4b","8b","16b"]},
+        {name: "counter", wave: "0.====0.", data: [0,1,2,3]},
+        {name: "o_fin",   wave: "0.....10",},
+
+    ]
+}
