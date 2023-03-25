78754fda54f0fd13fd9c218f3b76252d4cc1510a
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2023-03-25 20:22:23 +0800

    modify rsa.py

--- lab2/src/pc_python/golden/rsa.py
+++ lab2/src/pc_python/golden/rsa.py
@@ -2,26 +2,29 @@
 from sys import argv, stdin, stdout
 from struct import unpack, pack
 
+
 def mul_naive(a, b, n):
     ret = 0
     for i in range(255, -1, -1):
         ret <<= 1
         if ret >= n:
             ret -= n
-        if b & (1<<i):
+        if b & (1 << i):
             ret += a
             if ret >= n:
                 ret -= n
     return ret
 
+
 def power_naive(a, b, n):
     a2 = a + 0
     ret = 1
     for i in range(256):
-        if b & (1<<i):
+        if b & (1 << i):
             ret = mul_naive(ret, a2, n)
         a2 = mul_naive(a2, a2, n)
     return ret
+
 
 def mont_preprocess(a, n):
     """return a*2^(256) % n"""
@@ -33,26 +36,29 @@ def mont_preprocess(a, n):
     # return (a<<256)%n
     return a
 
+
 def mul_mont(a, b, n):
     """return a*b*2^(-256) % n"""
-    ret = 0 # Note: ret must has 257b [0,2n)
+    ret = 0  # Note: ret must has 257b [0,2n)
     for i in range(256):
-        if b & (1<<i):
+        if b & (1 << i):
             ret += a
         if ret & 1:
             ret += n
         ret >>= 1
-    return ret if ret<n else ret-n # [0,n) now
+    return ret if ret < n else ret-n  # [0,n) now
+
 
 def power_mont(a, b, n):
     a2 = mont_preprocess(a+0, n)
     ret = 1
     # print hex(ret)
     for i in range(256):
-        if b & (1<<i):
+        if b & (1 << i):
             ret = mul_mont(ret, a2, n)
         a2 = mul_mont(a2, a2, n)
     return ret
+
 
 if __name__ == '__main__':
     val_n = 0xCA3586E7EA485F3B0A222A4C79F7DD12E85388ECCDEE4035940D774C029CF831
@@ -63,25 +69,57 @@ if __name__ == '__main__':
         exponentiation = val_e
         r_chunk_size = 31
         w_chunk_size = 32
+        file_out = open('enc4.bin', 'wb')
+
+        while True:
+            chunk = stdin.read(r_chunk_size)
+            n_read = len(chunk)
+
+            if n_read < r_chunk_size:
+                if n_read != 0:
+                    print "There are {} trailing bytes left (ignored).".format(n_read)
+                break
+            else:
+                vals = unpack("{}B".format(r_chunk_size), chunk)
+                msg = 0
+                for val in vals:
+                    msg = (msg << 8) | val
+                # Choose one
+                # msg_new = power_naive(msg, exponentiation, val_n)
+                msg_new = power_mont(msg, exponentiation, val_n)
+                vals_new = map(lambda shamt: (msg_new >> shamt) &
+                               255, range((w_chunk_size-1)*8, -8, -8))
+                vals_new = pack("{}B".format(w_chunk_size), *vals_new)
+                stdout.write(vals_new)
+                file_out.write(vals_new)
+
     else:
         exponentiation = val_d
         r_chunk_size = 32
         w_chunk_size = 31
-    while True:
-        chunk = stdin.read(r_chunk_size)
-        n_read = len(chunk)
-        if n_read < r_chunk_size:
-            if n_read != 0:
-                print "There are {} trailing bytes left (ignored).".format(n_read)
-            break
-        else:
-            vals = unpack("{}B".format(r_chunk_size), chunk)
-            msg = 0
-            for val in vals:
-                msg = (msg<<8) | val
-            # Choose one
-            # msg_new = power_naive(msg, exponentiation, val_n)
-            msg_new = power_mont(msg, exponentiation, val_n)
-            vals_new = map(lambda shamt: (msg_new>>shamt)&255, range((w_chunk_size-1)*8, -8, -8))
-            vals_new = pack("{}B".format(w_chunk_size), *vals_new)
-            stdout.write(vals_new)
+        file_in = open('enc1.bin', 'rb')
+        file_out = open('dec1.bin', 'wb')
+
+        message = file_in.read(r_chunk_size)
+        while message:
+            chunk = message
+            n_read = len(chunk)
+            # print message
+            if n_read < r_chunk_size:
+                if n_read != 0:
+                    print "There are {} trailing bytes left (ignored).".format(n_read)
+                break
+            else:
+                vals = unpack("{}B".format(r_chunk_size), chunk)
+                msg = 0
+                for val in vals:
+                    msg = (msg << 8) | val
+                # Choose one
+                # msg_new = power_naive(msg, exponentiation, val_n)
+                msg_new = power_mont(msg, exponentiation, val_n)
+                vals_new = map(lambda shamt: (msg_new >> shamt) &
+                               255, range((w_chunk_size-1)*8, -8, -8))
+                vals_new = pack("{}B".format(w_chunk_size), *vals_new)
+                stdout.write(vals_new)
+                file_out.write(vals_new)
+            message = file_in.read(r_chunk_size)
