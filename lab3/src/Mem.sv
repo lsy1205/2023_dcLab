aff16d0d23db6db412d7e0fbee323dcf03854aa2
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2023-04-19 00:54:12 +0800

    modify mem.sv

--- lab3/src/Mem.sv
+++ lab3/src/Mem.sv
@@ -23,8 +23,8 @@ localparam S_IDLE = 0;
 localparam S_FETCH = 1;
 
 logic state_r, state_w;
-logic o_we_n_r, o_we_n_w;
-logic o_valid_r, o_valid_w;
+logic we_n_r, we_n_w;
+logic valid_r, valid_w;
 logic [19:0] data_length_r, data_length_w;
 logic [20:0] addr_now_r, addr_now_w;
 logic [15:0] record_data_r, record_data_w;
@@ -32,7 +32,7 @@ logic [15:0] record_data_r, record_data_w;
 assign o_SRAM_ADDR = addr_now_r;
 assign io_SRAM_DQ  = ( i_mode) ? record_data_r : 16'dz; // sram_dq as output
 assign o_r_data    = (!i_mode ) ?  io_SRAM_DQ : 16'd0; // sram_dq as input
-assign o_SRAM_WE_N = o_we_n_r;
+assign o_SRAM_WE_N = we_n_r;
 assign o_SRAM_CE_N = 1'b0;
 assign o_SRAM_OE_N = 1'b0;
 assign o_SRAM_LB_N = 1'b0;
@@ -42,8 +42,8 @@ always_comb begin
     state_w = state_r;
     case (state_r)
         S_IDLE: begin
-			if(i_start) state_w = S_FETCH;
-			else        state_w = S_IDLE;
+			if(i_start && valid_w) state_w = S_FETCH;
+			else                   state_w = S_IDLE;
         end 
 		S_FETCH: begin
 			state_w = S_IDLE;
@@ -58,55 +58,58 @@ always_comb begin
 	addr_now_w    = addr_now_r;
 	data_length_w = data_length_r;
 	record_data_w = record_data_r;
-	o_we_n_w      = o_we_n_r;
-	o_valid_w     = o_valid_r;
+	we_n_w        = we_n_r;
+	valid_w       = valid_r;
 	o_fin_w       = 0;
     case (state_r)
         S_IDLE: begin 
-			o_valid_w = 0;
+			valid_w = 0;
 			if(i_start) begin
 				if(i_mode) begin                 // write to memory
 					addr_now_w = addr_now_r + 1;
-					if(addr_now_w[20]) o_vaild_w = 0;
+					if(addr_now_w[20]) begin
+						o_fin_w = 1;
+						valid_w = 0;	
+					end
 					else begin
-						o_we_n_w      = 0;	
-						o_vaild_w     = 1;
+						we_n_w        = 0;	
+						valid_w       = 1;
 						record_data_w = i_w_data;
 						data_length_w = data_length_r + 1;
 					end
 				end
 				else begin                       // read from memory
-					addr_now_w = (addr_now_r == 21'h1fffff) ? addr_now_r + 1 : addr_now_r + (i_next_num + 1);
-					if(addr_now_w[20] || addr_now_w >= data_length_r) o_valid_w = 0;
-					else begin
-						o_valid_w = 1;
-					end
+					addr_now_w = (addr_now_r + 1) + 
+								 (addr_now_r != 21'h1fffff) ? i_next_num : 0;
+					valid_w    = (!addr_now_w[20] && addr_now_w < data_length_r) ;
+					if (!valid_w) o_fin_w = 1;
+					else          o_fin_w = 0;
 				end
 			end
 			else begin
-				
+				o_fin_w = 0;
 			end
         end 
 		S_FETCH: begin
 			if(i_mode) begin          // write to memory
-				if(o_valid_r) begin
-					o_valid_w = 1;
+				if(valid_r) begin
+					valid_w = 1;
 					o_fin_w   = 1;
-					o_we_n_w  = 1;
+					we_n_w  = 1;
 				end
 				else begin
-					o_valid_w = 0;
-					o_we_n_w  = 0;
+					valid_w = 0;
+					we_n_w  = 0;
 					o_fin_w   = 1; 
 				end		
 			end
 			else begin 		         // read from memory
 				o_fin_w = 1;			
-				if(o_valid_r) begin
-					o_valid_w = 1;
+				if(valid_r) begin
+					valid_w = 1;
 				end
 				else begin
-					o_vaild_w = 0;
+					valid_w = 0;
 				end
 			end
 		end
@@ -114,8 +117,8 @@ always_comb begin
 			addr_now_w    = addr_now_r;
 			data_length_w = data_length_r;
 			record_data_w = record_data_r;
-			o_we_n_w      = o_we_n_r;
-			o_valid_w     = o_valid_r;
+			we_n_w      = we_n_r;
+			valid_w     = valid_r;
 			o_fin_w       = 0;
 		end
     endcase
@@ -124,8 +127,8 @@ end
 always_ff @(posedge i_clk or negedge i_rst_n)begin
 	if(!i_rst_n || i_clear) begin
 		state_r       <= S_IDLE;
-		o_we_n_r      <= 1;
-		o_valid_r     <= 0;
+		we_n_r      <= 1;
+		valid_r     <= 0;
 		addr_now_r    <= 0;
 		record_data_r <= 0;
 		addr_now_r    <= 0;
@@ -133,8 +136,8 @@ always_ff @(posedge i_clk or negedge i_rst_n)begin
 	end
 	else begin
 		state_r       <= state_w;
-		o_we_n_r      <= o_we_n_w;
-		o_valid_r     <= o_valid_w;
+		we_n_r        <= we_n_w;
+		valid_r       <= valid_w;
 		data_length_r <= data_length_w;
 		addr_now_r    <= addr_now_w;
 		record_data_r <= record_data_w;
